6bde57f5e551756b48b5c12149eb8d2ca58765ad
kassette <50966326+mirkka-puente@users.noreply.github.com>
2022-10-11 13:31:24 +0200

    Update Mirkka_Puente_R-exercise.R

--- Mirkka_Puente_R-exercise.R
+++ Mirkka_Puente_R-exercise.R
@@ -40,10 +40,38 @@ d0$tree.name[d0$Girth == max(d0$Girth)]
 
 #Diameter
 MD <- mean(d0$Girth)
+#Volume
 MV <- mean(d0$Volume)
+#Height
 MH <- mean(d0$Height)
 
-max_volume <- d0$Volume > MV
+#6. Make a new factor variable into d0 with two levels: “Large” 
+#for trees with volume larger or equal the mean and “Small” with 
+#trees with volume smaller than the mean. ¿How many “Large” trees are there?
+
+shor_big_than <- d0$Volume > MV
+
+d0$Large_Small[shor_big_than] <- "Large"
+d0$Large_Small[!shor_big_than] <- "Small"
+
+#7. Make a new factor variable into d0 with “Tall” for trees taller or equal
+#the mean and “Short” with trees with Height shorter than the mean.
+
+tall_short_than <- d0$Height > MH
+
+d0$Tall_Short[tall_short_than] <- "Tall"
+d0$Tall_Short[!tall_short_than] <- "Short"
+
+#8. Make a subset with the trees that are both short and large 
+#and calculate the mean diameter of these Short-Large trees in meters. 
+#How many “Short_Large” trees are there?
+
+short_large <- (d0$Volume) >= MV & d0$Height < MH
+
+subset <- d0[short_large, ]
+
+m <- mean(subset$Girth)
+
 
 
 
